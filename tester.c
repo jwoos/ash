daffe56f363686b32adcea852a783936090d3a49
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-06-13 00:53:09 -0400

    remove sds from test

--- tester.c
+++ tester.c
@@ -7,28 +7,7 @@
 #include "shell.h"
 
 int main(int argc, char* argv[]) {
-/*
- *    Vector* v = vectorInitialize();
- *    int* a = malloc(sizeof(int));
- *    *a = 10;
- *    char* b = malloc(sizeof(char));
- *    *b = 'c';
- *    sds c = sdsnew("hello");
- *
- *    vectorPush(v, a);
- *    vectorPush(v, b);
- *    vectorPush(v, c);
- *
- *    printf("%d\n", v -> size);
- *    printf("%d\n", v -> capacity);
- *    printf("%d\n", *(int*) vectorGet(v, 0));
- *    printf("%c\n", *(char*) vectorGet(v, 1));
- *    printf("%s\n", (sds) vectorGet(v, 2));
- */
-
 	writeStdout("test\n", 5);
-	sds str = readStdin();
-	writeStdout(str, sdslen(str));
 	writeStdout("\n", 1);
 
 	_exit(EXIT_SUCCESS);
